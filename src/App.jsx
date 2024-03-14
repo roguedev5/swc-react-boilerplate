a90b85acb4d73009e78fc0091610daf7ac925923
roguedev5 <roguedev05@gmail.com>
2024-03-14 14:30:06 +0530

    feat: add logos and app info

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "./App.css";
+import logo from "../public/assets/logo.svg";
+import swc from "../public/assets/swc.svg";
+import webpack from "../public/assets/webpack.svg";
+import docker from "../public/assets/docker.svg";
 
 function App() {
-  return <div>React App based on SWC compiler...</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          A simple React Boiler plate app built using SWC(Speedy web compiler),
+          Webpack and Docker.
+        </p>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={swc} className="App-logo" alt="swc" />
+          <img src={webpack} className="App-logo" alt="webpack" />
+          <img src={docker} className="App-logo" alt="docker" />
+        </div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
